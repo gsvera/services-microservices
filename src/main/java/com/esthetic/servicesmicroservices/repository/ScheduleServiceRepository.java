e45a1d0b9d0500f9f0f4e0f2596f6fd900f6484c
gsvera <55720644+gsvera@users.noreply.github.com>
2025-09-27 13:56:41 -0500

    fix / refactor en consulta de citas por proveedor

--- src/main/java/com/esthetic/servicesmicroservices/repository/ScheduleServiceRepository.java
+++ src/main/java/com/esthetic/servicesmicroservices/repository/ScheduleServiceRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface ScheduleServiceRepository extends JpaRepository<ScheduleService, Long> {
     @Query(value = "SELECT s FROM ScheduleService s LEFT JOIN FETCH s.idClient c WHERE s.idProvider.id = ?1 AND s.scheduleDate = ?2 AND (c IS NOT NULL) ")
     List<ScheduleService> findByIdProvider(String idProvider, LocalDateTime date);
-    @Query(value = "SELECT s FROM ScheduleService s LEFT JOIN FETCH s.idClient c WHERE s.idProvider.id = ?1 AND s.scheduleDate = ?2 AND statusService = ?3 AND (c IS NOT NULL)")
+    @Query(value = "SELECT s FROM ScheduleService s JOIN FETCH s.idClient c WHERE s.idProvider.id = ?1 AND s.scheduleDate = ?2 AND statusService = ?3")
     List<ScheduleService> findByIdProviderAndStatusService(String idProvider, LocalDateTime date, Integer statusService);
     @Query(value = "SELECT s FROM ScheduleService s WHERE idProvider.id  = ?1 AND scheduleDate = ?2 AND statusService NOT IN (-1, 2)")
     List<ScheduleService> findScheduleByProvider(String idProvider, LocalDateTime date);
